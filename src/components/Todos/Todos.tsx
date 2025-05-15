b38826deeb8ed7f7523e5ac2ff4baede09c2bdba
kkgomez2 <kkristoffergomez@gmail.com>
2025-05-14 19:37:34 -0700

    Basic add and remove logic for reducer

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
+import { connect } from "react-redux";
+import { v4 as uuidv4 } from 'uuid';
+import { addTodos, removeTodos } from "../../redux/reducer";
 
-const Todos = () => {
+const mapStateToProps = (state: any) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    addTodo: (obj: any) => dispatch(addTodos(obj)),
+    removeTodo: (id: string) => dispatch(removeTodos(id))
+  };
+};
+
+const Todos = (props: any) => {
   const [todo, setTodo] = useState("");
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(e.target.value);
   };
-  console.log(todo);
+  console.log("props from store", props);
 
   return (
     <div>
@@ -16,9 +32,32 @@ const Todos = () => {
         className="todo-input"
       />
 
-      <button className="add-btn">Add</button>
+      <button
+        className="add-btn"
+        onClick={() =>
+          props.addTodo({
+            id: uuidv4(),
+            item: todo,
+            completed: false,
+          })
+        }
+      >
+        Add
+      </button>
+      <br/>
+
+      <ul>
+        {props.todos.map((item:any) => {
+          return <li key={item.id}>{item.item}
+            <button
+              className="remove-btn"
+              onClick={() => props.removeTodo(item.id)}
+            >Remove</button>
+          </li>
+        })}
+      </ul>
     </div>
   );
 };
 
-export default Todos;
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
---
+++ src/redux/reducer.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState: any = [];
+
+const addTodoReducer = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodos: (state, action) => {
+      state.push(action.payload);
+      return state;
+    },
+    removeTodos: (state, action) => {
+      return state.filter((item:any) => item.id !== action.payload)
+    },
+    updateTodos: (state, action) => {
+      return state.map((todo:any) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            item: action.payload.item
+          }
+        } else {
+          return todo;
+        }
+      })
+    }
+  },
+});
+
+export const { addTodos, removeTodos } = addTodoReducer.actions;
+export const reducer = addTodoReducer.reducer;
